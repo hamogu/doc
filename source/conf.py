82d2fff49778ae1c9fd057bbe049253a297276f0
hamogu <hgunther@mit.edu>
2014-12-18 15:54:34 -0500

    fix copyright to MIT

--- source/conf.py
+++ source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MARX'
-copyright = u'2014, Hans Moritz Günther for the MARX team'
+copyright = u'2014, Massachusetts Institute of Technology'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
